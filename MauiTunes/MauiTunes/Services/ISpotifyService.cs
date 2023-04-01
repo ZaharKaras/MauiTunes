d259d2bdf1c32828c9010f2ee94e56b08fc54473
ZaharKaras <93844318+ZaharKaras@users.noreply.github.com>
2023-04-01 03:22:35 +0300

    Added spotify service

---
+++ MauiTunes/MauiTunes/Services/ISpotifyService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiTunes.Services
+{
+    public interface ISpotifyService
+    {
+        Task<Album> GetAlbum(string albumName);
+        Task<Artis> GetArtis(string artistName);
+        Task<Track> GetTrack(string trackName);
+    }
+}
---
+++ MauiTunes/MauiTunes/Services/SpotifyService.cs
@@ -0,0 +1,30 @@
+﻿using MauiTunes.Entities;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiTunes.Services
+{
+    public class SpotifyService : ISpotifyService
+    {
+        private readonly RestClient _client;
+        public Task<Album> GetAlbum(string albumName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Artist> GetArtis(string artistName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Track> GetTrack(string trackName)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}

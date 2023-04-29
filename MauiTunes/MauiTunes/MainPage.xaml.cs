384d1f786a6e1e300bccc7b818b014294d538e7a
ZaharKaras <93844318+ZaharKaras@users.noreply.github.com>
2023-04-29 22:44:52 +0300

    Update mainpage

--- MauiTunes/MauiTunes/MainPage.xaml.cs
+++ MauiTunes/MauiTunes/MainPage.xaml.cs
@@ -1,31 +1,37 @@
-﻿using MauiTunes.Services;
+﻿using MauiTunes.Entities;
+using MauiTunes.Models;
+using MauiTunes.Services;
 using SpotifyAPI.Web;
+using System.Collections.ObjectModel;
 
 namespace MauiTunes;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-	private ISpotifyAccountService _spotifyAccountService { get; init; }
-	private ISpotifyService _spotifyService { get; init; }
+	private ISpotifyAccountService _spotifyAccountService;
+	private ISpotifyService _spotifyService;
+	public ObservableCollection<SearchResult> Art { get; set; }
 
-	public MainPage(ISpotifyAccountService spotifyAccountService, ISpotifyService spotifyService)
+    public MainPage(ISpotifyAccountService spotifyAccountService, ISpotifyService spotifyService)
 	{
 		InitializeComponent();
 		_spotifyAccountService = spotifyAccountService;
 		_spotifyService = spotifyService;
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+		var token = await _spotifyAccountService.GetToken("541e4eb4340743ef80eb76cb7bace234", "2114609948734fc9b529f44c50f3d3cc");
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+		//var result = await _spotifyService.GetArtist("three days grace", token);
+		var result = await _spotifyService.GetTracks("i hate everything about you", token);
+		//var artists = result.Artists.Items;
+		var tracks = result.Tracks.Items;
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+		var observableArtist = new ObservableCollection<SearchResult>();
+		Art = observableArtist;
+		albumsList.ItemsSource = tracks;
+
+    }
 }
 

ea1f22d72dd10239bab7223f344c5b8f1f490ada
ZaharKaras <93844318+ZaharKaras@users.noreply.github.com>
2023-05-14 21:40:15 +0300

    add converters

---
+++ MauiTunes/MauiTunes/Converters/ViewToColumnWidthConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiTunes.Converters
+{
+    public class ViewToColumnWidthConverter : IValueConverter
+    {
+        public int NumberOfColumns { get; set; }
+        public double Deducation { get; set; }
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var view = (View)value;
+
+            var width = view.Width - Deducation;
+
+            return width / NumberOfColumns;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

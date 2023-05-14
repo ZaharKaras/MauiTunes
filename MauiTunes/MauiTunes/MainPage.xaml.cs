2a20c70346bf255436cbd163555780883209acf3
ZaharKaras <93844318+ZaharKaras@users.noreply.github.com>
2023-05-14 21:43:48 +0300

    update build files

--- MauiTunes/MauiTunes/MainPage.xaml.cs
+++ MauiTunes/MauiTunes/MainPage.xaml.cs
@@ -15,6 +15,7 @@ public partial class MainPage : ContentPage
 	private ISpotifyPlayer _spotifyPlayer;
 	private List<Track> _tracks;
 	private AuthorizationToken token;
+	private int count = 0;
 	public ObservableCollection<SearchResult> Art { get; set; }
 
     public MainPage(ISpotifyAccountService spotifyAccountService, ISpotifyService spotifyService, ISpotifyPlayer spotifyPlayer)
@@ -29,7 +30,7 @@ public partial class MainPage : ContentPage
     {
 		token = await _spotifyAccountService.GetToken("541e4eb4340743ef80eb76cb7bace234", "2114609948734fc9b529f44c50f3d3cc");
 
-		var result = await _spotifyService.GetAlbums("three days grace", token);
+		var result = await _spotifyService.GetTracks("three days grace", token);
 		_tracks = result.Tracks.Items;
 
 		var observableArtist = new ObservableCollection<SearchResult>();
@@ -40,7 +41,18 @@ public partial class MainPage : ContentPage
 
 	private async void Button_Clicked_1Async(object sender, EventArgs e)
 	{
+		var preview_url = _tracks[0].PreviewUrl;
+		if(count == 1)
+		{
+			_spotifyPlayer.StopeAudioAsync();
+			count--;
+		}
+		else
+		{
+            count++;
+            _spotifyPlayer.PlayAudioAsync();
 
+        }
     }
 }
 
